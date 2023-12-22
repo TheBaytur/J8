4a6cc48e0739704ed9b3aab4c13f46deaee4eee7
TheBaytur <bayturcodding@gmail.com>
2023-12-22 16:00:32 +0800

    added elements

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,8 @@ public class Main { //Collections
         System.out.println(list.isEmpty());
         list.add("first");
         list.add("second");
+        list.add("third");
+
         for (String string : list){
             System.out.println(string);
         }
@@ -15,6 +17,14 @@ public class Main { //Collections
         list.remove("first");
         for (int i = 0; i < list.size(); i++){
             System.out.println("i " + i + " item " + list.get((i)));
+        }
+
+        list.add(0, "forth");
+        list.add("fifth");
+        System.out.println("added 2 items");
+        list.remove(list.size() - 1);
+        for (int i = 0; i < list.size(); i++){
+            System.out.println("i " + i + " item " + list.get(i));
         }
     }
 }

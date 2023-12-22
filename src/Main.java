f3e72274f1cf7cd7d048fbe0c48b2862f5d0a0eb
TheBaytur <bayturcodding@gmail.com>
2023-12-22 18:19:37 +0800

    New item

--- src/Main.java
+++ src/Main.java
@@ -5,26 +5,31 @@ public class Main { //Collections
     public static void main(String[] args) {
 
         List<String> list = new ArrayList<>();
-        System.out.println(list.isEmpty());
+
         list.add("first");
         list.add("second");
         list.add("third");
+        list.add("forth");
+        list.add("fifth");
+        list.add("so");
+        list.clear();
+        list.add("new item");
 
         for (String string : list){
             System.out.println(string);
         }
 
-        list.remove("first");
-        for (int i = 0; i < list.size(); i++){
-            System.out.println("i " + i + " item " + list.get((i)));
-        }
-
-        list.add(0, "forth");
-        list.add("fifth");
-        System.out.println("added 2 items");
-        list.remove(list.size() - 1);
-        for (int i = 0; i < list.size(); i++){
-            System.out.println("i " + i + " item " + list.get(i));
-        }
+//        list.remove("first");
+//        for (int i = 0; i < list.size(); i++){
+//            System.out.println("i " + i + " item " + list.get((i)));
+//        }
+//
+//        list.add(0, "forth");
+//        list.add("fifth");
+//        System.out.println("added 2 items");
+//        list.remove(list.size() - 1);
+//        for (int i = 0; i < list.size(); i++){
+//            System.out.println("i " + i + " item " + list.get(i));
+//        }
     }
 }
